6d4d11e9c0e4784108c8534c42db932fc5229f25
Shomzyt <43217303+Shomzyt@users.noreply.github.com>
2018-12-10 12:19:58 -0500

    Update server.js
    Testing server fix

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ var APP_PATH = path.join(__dirname, 'dist');
 
 app.set('port', (process.env.PORT || 3000));
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 app.use('/', express.static(APP_PATH));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
