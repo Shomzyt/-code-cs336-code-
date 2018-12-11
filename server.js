1ddaaad2f2833a7479613a5ab40a33c3c4889bae
Shomzyt <43217303+Shomzyt@users.noreply.github.com>
2018-12-11 12:33:58 -0500

    Update server.js
    Using Vlinden's code.

--- server.js
+++ server.js
@@ -10,72 +10,94 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
-var MongoClient = require('mongodb').MongoClient
-var password = process.env.MONGO_PASSWORD;
 var db;
-var data;
-
-
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+var APP_PATH = path.join(__dirname, 'dist');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(APP_PATH));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest comments.
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
 
 app.get('/api/comments', function(req, res) {
-    var collection = db.collection('labcomments');
-    collection.find({}).toArray(function (err, docs) {
-        res.json(docs)
-    })
+    db.collection("comments").find({}).toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
+    });
 });
 
 app.post('/api/comments', function(req, res) {
-     var collection = db.collection('labcomments');
-     collection.insertOne({
+    var newComment = {
         id: Date.now(),
         author: req.body.author,
         text: req.body.text,
-     },
-     function (err, result) {
-        console.log("added comment");
-        res.json((result));
-     });
+    };
+    db.collection("comments").insertOne(newComment, function(err, result) {
+        if (err) throw err;
+        db.collection("comments").find({}).toArray(function(err, docs) {
+            if (err) throw err;
+            res.json(docs);
+        });
+    });
 });
 
-// lab10code
-MongoClient.connect('mongodb://cs336:' + password + '@ds227674.mlab.com:27674/cs336', function (err, client) {
-    
-  if (err) {
-    throw err
-    }
-
-  db = client.db("cs336");
-  
-  db.collection('labcomments').find().toArray(function (err, result) {
-    if (err) throw er
-        data = result;
+app.get('/api/comments/:id', function(req, res) {
+    db.collection("comments").find({"id": Number(req.params.id)}).toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
     });
+});
 
-    app.listen(app.get('port'), function() {
-        console.log('Server started: http://localhost:' + app.get('port') + '/');
-    })
+app.put('/api/comments/:id', function(req, res) {
+    var updateId = Number(req.params.id);
+    var update = req.body;
+    db.collection('comments').updateOne(
+        { id: updateId },
+        { $set: update },
+        function(err, result) {
+            if (err) throw err;
+            db.collection("comments").find({}).toArray(function(err, docs) {
+                if (err) throw err;
+                res.json(docs);
+            });
+        });
+});
+
+app.delete('/api/comments/:id', function(req, res) {
+    db.collection("comments").deleteOne(
+        {'id': Number(req.params.id)},
+        function(err, result) {
+            if (err) throw err;
+            db.collection("comments").find({}).toArray(function(err, docs) {
+                if (err) throw err;
+                res.json(docs);
+            });
+        });
+});
+
+app.use('*', express.static(APP_PATH));
+
+app.listen(app.get('port'), function() {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
+
+// This assumes that the MongoDB password has been set as an environment variable.
+var mongoURL = 'mongodb://cs336:' +
+	       process.env.MONGO_PASSWORD +
+           '@ds145667.mlab.com:45667/cs336';
+MongoClient.connect(mongoURL, function(err, dbConnection) {
+    if (err) throw err;
+    db = dbConnection;
 });
